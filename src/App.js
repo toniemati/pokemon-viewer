b0678445a8dbfa787a358a41ad47a68a46bce649
toniemati <matikas2000@gmail.com>
2021-12-21 15:01:54 +0100

    setup router and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './views/Home/Home';
+import About from './views/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css';
+import { Link, useLocation } from 'react-router-dom';
+
+const Header = () => {
+  const location = useLocation();
+
+  const checkPath = (path) => location.pathname === path;
+
+  console.log('value: ', checkPath('/'));
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <h2>Pokemon Viewer</h2>
+      </div>
+
+      <div className="header__right">
+        <Link className={ checkPath('/') && 'active__link'} to="/">Home</Link>
+        <span> | </span>
+        <Link className={ checkPath('/about') && 'active__link' } to="about">About</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
